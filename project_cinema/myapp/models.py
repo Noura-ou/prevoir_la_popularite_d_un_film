47b609abd9d91e4188c87183d011980a887c2371
RolaMmss <rola.el-moallem@hotmail.fr>
2023-07-26 14:11:51 +0200

    model film added

--- project_cinema/myapp/models.py
+++ project_cinema/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    titre = models.CharField(max_length=100)
+    date_de_sortie = models.DateField()
+    duree = models.IntegerField()  # Durée en minutes
+    genre = models.CharField(max_length=50)
+    nombre_entrees_estimees = models.IntegerField(default=0)  # Champ pour stocker l'estimation du nombre d'entrées en première semaine
+
+    def __str__(self):
+        return self.titre
