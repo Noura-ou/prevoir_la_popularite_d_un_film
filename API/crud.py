f080bc2fcd6e5d8f7cf45a4bebf045ffb065b733
amineelbb <amineee59@hotmail.fr>
2023-08-02 16:46:35 +0200

    apiaid

--- API/crud.py
+++ API/crud.py
@@ -4,67 +4,90 @@ import pyodbc
 from dotenv import load_dotenv
 import os
 import pandas as pd
+import ast 
+import re
 
 # Charger les variables d'environnement à partir du fichier .env
+
+
+# def clean_data(data):
+#     # Remplacer les valeurs manquantes par des zéros
+#     data = data.dropna()
+
+#     for col in data.columns:
+#         data[col] = data[col].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+
+
+#     # Supprimer les doublons
+#     data = data.drop_duplicates()
+
+#     return data
+
+
+# def update_actors_count_from_azure_db():
 load_dotenv()
-
-# Utiliser les variables d'environnement chargées
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_server = os.getenv("DB_SERVER")
-db_name = os.getenv("DB_NAME")
-db_driver = os.getenv("DB_DRIVER")
+username = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+server = os.getenv('DB_SERVER')
+database = os.getenv('DB_name')
+DB_Driver = os.getenv('DB_Driver')
 
 
-def clean_data(data):
-    # Remplacer les valeurs manquantes par des zéros
-    data = data.dropna()
 
-    for col in data.columns:
-        data[col] = data[col].apply(lambda x: x.translate(str.maketrans('', '', string.punctuation)))
+# Établissez la connexion à votre base de données
+connection_string = f'Driver={DB_Driver};Server=tcp:{server},1433;Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+conn = pyodbc.connect(connection_string)
 
-
-    # Supprimer les doublons
-    data = data.drop_duplicates()
-
-    return data
-
-
-def update_actors_count_from_azure_db():
-    # Connexion à la base de données Azure SQL
-    conn = pyodbc.connect(f"DRIVER={db_driver};SERVER={db_server};DATABASE={db_name};UID={db_user};PWD={db_password}")
-    cursor = conn.cursor()
+cursor = conn.cursor()
 
     # Exécuter une requête SQL pour récupérer les données
-    query ="""
-    SELECT acteurs_films.*, COUNT(acteurs_films.acteurs_connu) as nombre_acteur_connu
-    FROM acteurs_films
-    INNER JOIN films ON acteurs_films.film_id = films.id
-    GROUP BY acteurs_films.id, acteurs_films.film_id, acteurs_films.acteurs_connu, films.id;
+    # query ="""
+    # SELECT acteurs_films.*, COUNT(acteurs_films.acteurs_connu) as nombre_acteur_connu
+    # FROM acteurs_films
+    # INNER JOIN films ON acteurs_films.film_id = films.id
+    # GROUP BY acteurs_films.id, acteurs_films.film_id, acteurs_films.acteurs_connu, films.id;
 
+    # """
+df_acteurs_connus = pd.read_csv('API/top_acteurs.csv')
+
+acteurs_connus = set(df_acteurs_connus['acteur'])
+
+
+query= """ SELECT titre,duree, distributeur,realisateur,nationalites, langue_d_origine,
+       type_film, annee_production, acteurs_films.acteurs
+            FROM films
+            INNER JOIN acteurs_films ON films.id = acteurs_films.film_id;
     """
 
-    cursor.execute(query)
-    results = cursor.fetchall()
+    # cursor.execute(query)
+    # results = cursor.fetchall()
 
-    # Fermer la connexion
-    conn.close()
    
 
-    # df_azure_data = pd.read_sql(query, conn)
+df_azure_data = pd.read_sql(query, conn)
 
 
-    # df_azure_data['nombre_acteurs_connus'] = df_azure_data['acteurs'].apply(lambda x: len([acteur for acteur in eval(x) if acteur in df_azure_data]))
+df_azure_data['nombre_acteurs_connus'] = df_azure_data['acteurs'].apply(lambda x: len([acteur for acteur in eval(x) if acteur in acteurs_connus]))
+# df_azure_data['nombre_acteurs_connus'] = df_azure_data['acteurs'].apply(lambda x: len([acteur for acteur in eval(x) if acteur in df_acteurs_connus]))
 
-    # # df_azure_data['nombre_acteurs_connus'] = df_azure_data['acteurs_connu'].apply(lambda x: x.count(',') + 1 if isinstance(x, str) else 0)
+#df_azure_data['nombre_acteurs_connus'] = acteurs_connus.apply(lambda x: x.count(',') + 1 if isinstance(x, str) else 0)
 
-# Compter le nombre d'acteurs connus dans chaque film et créer une nouvelle colonne pour cela
+# Utiliser ast.literal_eval() pour convertir les chaînes de caractères en listes d'acteurs
 
-
-    # Fermer la connexion à la base de données
-    # conn.close()
-
-    # return df_azure_data
+# Utiliser la fonction extract_actors pour extraire les noms d'acteurs de la colonne acteurs
 
 
 
+    
+   
+conn.close()
+#return df_azure_data
+print(df_azure_data)
+
+
+
+
+  
+
+
+ 
