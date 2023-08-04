a90d4460b9af923630201fb5e9b435dd13f92f3e
amineelbb <amineee59@hotmail.fr>
2023-08-05 01:55:11 +0200

    apifini

--- API/crud.py
+++ API/crud.py
@@ -1,3 +1,89 @@
+# from fastapi import HTTPException
+# import string 
+# import pyodbc
+# from dotenv import load_dotenv
+# import os
+# import pandas as pd
+# import ast 
+# import unicodedata
+# import re
+
+# # def update_from_azure_db():
+#         load_dotenv()
+#         username = os.getenv('DB_USER')
+#         password = os.getenv('DB_PASSWORD')
+#         server = os.getenv('DB_SERVER')
+#         database = os.getenv('DB_name')
+#         DB_Driver = os.getenv('DB_Driver')
+
+
+
+# # Établissez la connexion à votre base de données
+#         connection_string = f'Driver={DB_Driver};Server=tcp:{server},1433;Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+#         conn = pyodbc.connect(connection_string)
+
+#         cursor = conn.cursor()
+    
+#     # try:
+#         conn = pyodbc.connect(connection_string)
+#         cursor = conn.cursor()
+
+#         query = """ SELECT titre,
+#                    MAX(duree) AS duree,
+#                    MAX(distributeur) AS distributeur,
+#                    MAX(realisateur) AS realisateur,
+#                    MAX(nationalites) AS nationalites,
+#                    MAX(langue_d_origine) AS langue_d_origine,
+#                    MAX(type_film) AS type_film,
+#                    MAX(genre) AS genre,
+#                    MAX(annee_production) AS annee_production,
+#                    STRING_AGG(acteurs, ',') AS acteurs,
+#                    STRING_AGG(top_acteurs.acteur, ',') AS acteur
+#                    FROM films
+#                    INNER JOIN acteurs_films ON films.id = acteurs_films.film_id
+#                    INNER JOIN top_acteurs ON acteurs_films.id_acteurs_films = top_acteurs.id
+#                    GROUP BY titre;
+#         """
+
+#         df_azure_data = pd.read_sql(query, conn)
+
+#         # Fermer la connexion après utilisation
+#         conn.close()
+
+#         # Fonction pour nettoyer le nom d'un acteur
+#         def clean_name(name):
+#             name = name.lower()  # Convertir en minuscules
+#             name = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode('utf-8')  # Supprimer les accents
+#             name = name.replace(" ", "")  # Supprimer les espaces
+#             return name
+
+#         # Calculer le nombre d'acteurs connus
+#         def calculate_known_actors(row):
+#             actor = clean_name(row['acteur'][0])
+#             actors = [clean_name(a) for a in row['acteurs']]
+
+#             return sum(actor in a for a in actors)
+
+#         def calculate_known_realisateur(row):
+#             actor = clean_name(row['acteur'][0])
+#             realisateur = [clean_name(a) for a in row['realisateur']]
+
+#             return int(any(actor in a for a in realisateur))
+
+#         df_azure_data['nombre_acteurs_connus'] = df_azure_data.apply(calculate_known_actors, axis=1)
+#         df_azure_data['realisateur_connu'] = df_azure_data.apply(calculate_known_realisateur, axis=1)
+
+#         unique_genres = set(g for row in df_azure_data['genre'] for g in row)
+
+#         for genre in unique_genres:
+#             df_azure_data[genre] = df_azure_data['genre'].apply(lambda x: 1 if genre in x else 0)
+
+        # return df_azure_data
+
+    # except pyodbc.Error as err:
+    #     raise HTTPException(status_code=500, detail="Error connecting to the Azure database")
+#return df_azure_dat
+
 from fastapi import HTTPException
 import string 
 import pyodbc
@@ -8,82 +94,99 @@ import ast
 import unicodedata
 import re
 
+
 def update_from_azure_db():
-    load_dotenv()
-    username = os.getenv('DB_USER')
-    password = os.getenv('DB_PASSWORD')
-    server = os.getenv('DB_SERVER')
-    database = os.getenv('DB_name')
-    DB_Driver = os.getenv('DB_Driver')
-
-
-
-# Établissez la connexion à votre base de données
-    connection_string = f'Driver={DB_Driver};Server=tcp:{server},1433;Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-    conn = pyodbc.connect(connection_string)
-
-    cursor = conn.cursor()
-    
     try:
-        conn = pyodbc.connect(connection_string)
-        cursor = conn.cursor()
+        # Load environment variables
 
+
+# Load environment variables
+        load_dotenv()
+        username = os.getenv('DB_USER')
+        password = os.getenv('DB_PASSWORD')
+        server = os.getenv('DB_SERVER')
+        database = os.getenv('DB_name')
+        DB_Driver = os.getenv('DB_Driver')
+
+# Établir la connexion à votre base de données
+        connection_string = f'Driver={DB_Driver};Server=tcp:{server},1433;Database={database};Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+        conn = pyodbc.connect(connection_string)
+
+        cursor = conn.cursor()
+# MAX(genre) AS genres,
+ # STRING_AGG(genre, '_') AS genres,
+# Execute the SQL query and fetch data into a DataFrame
         query = """ SELECT titre,
-                   MAX(duree) AS duree,
-                   MAX(distributeur) AS distributeur,
-                   MAX(realisateur) AS realisateur,
-                   MAX(nationalites) AS nationalites,
-                   MAX(langue_d_origine) AS langue_d_origine,
-                   MAX(type_film) AS type_film,
-                   MAX(genre) AS genre,
-                   MAX(annee_production) AS annee_production,
-                   STRING_AGG(acteurs, ',') AS acteurs,
-                   STRING_AGG(top_acteurs.acteur, ',') AS acteur
-                   FROM films
-                   INNER JOIN acteurs_films ON films.id = acteurs_films.film_id
-                   INNER JOIN top_acteurs ON acteurs_films.id_acteurs_films = top_acteurs.id
-                   GROUP BY titre;
-        """
+           MAX(duree) AS durée,
+           MAX(distributeur) AS distributeur,
+           MAX(realisateur) AS réalisateur,
+           MAX(nationalites) AS nationalités,
+           MAX(langue_d_origine) AS langue_d_origine,
+           MAX(type_film) AS type_film,
+           MAX(genre) AS genres,
+           MAX(annee_production) AS annee_production,
+           STRING_AGG(acteurs, ',') AS acteurs,
+           STRING_AGG(top_acteurs.acteur, ',') AS acteurs_connus
+           FROM films
+           INNER JOIN acteurs_films ON films.id = acteurs_films.film_id
+           INNER JOIN top_acteurs ON acteurs_films.id_acteurs_films = top_acteurs.id
+           GROUP BY titre;
+            """
 
         df_azure_data = pd.read_sql(query, conn)
 
-        # Fermer la connexion après utilisation
+# Fermer la connexion après utilisation
         conn.close()
 
-        # Fonction pour nettoyer le nom d'un acteur
+# Fonction pour nettoyer le nom d'un acteur
         def clean_name(name):
             name = name.lower()  # Convertir en minuscules
             name = unicodedata.normalize('NFKD', name).encode('ASCII', 'ignore').decode('utf-8')  # Supprimer les accents
             name = name.replace(" ", "")  # Supprimer les espaces
             return name
 
-        # Calculer le nombre d'acteurs connus
+# Calculer le nombre d'acteurs connus
         def calculate_known_actors(row):
-            actor = clean_name(row['acteur'][0])
+            actor = clean_name(row['acteurs_connus'][0])
             actors = [clean_name(a) for a in row['acteurs']]
 
             return sum(actor in a for a in actors)
 
         def calculate_known_realisateur(row):
-            actor = clean_name(row['acteur'][0])
-            realisateur = [clean_name(a) for a in row['realisateur']]
+            actor = clean_name(row['acteurs_connus'][0])
+            realisateur = [clean_name(a) for a in row['réalisateur']]
 
             return int(any(actor in a for a in realisateur))
 
         df_azure_data['nombre_acteurs_connus'] = df_azure_data.apply(calculate_known_actors, axis=1)
         df_azure_data['realisateur_connu'] = df_azure_data.apply(calculate_known_realisateur, axis=1)
+# df_azure_data['genres'] = df_azure_data['genres'].apply(lambda x: [genre.strip() for genre in x.split('_')])# print(df_azure_data['genres'])
+# df_azure_data['genres'] = df_azure_data['genres'].apply(lambda x: [genre.strip() for genre in x.split('_')])
 
-        unique_genres = set(g for row in df_azure_data['genre'] for g in row)
+# df_azure_data['genres'] = df_azure_data['genres'].apply(lambda x: [x])
+# print(df_azure_data["genres"])
+
+
+
+# print(df_azure_data["genres"])
+
+        unique_genres = set(g for row in df_azure_data['genres'] for g in row)
 
         for genre in unique_genres:
-            df_azure_data[genre] = df_azure_data['genre'].apply(lambda x: 1 if genre in x else 0)
+
+            df_azure_data[genre] = df_azure_data['genres'].apply(lambda x: 1 if genre in x else 0)
+
+
+# print(df_azure_data.columns)
+
+# print(len(df_azure_data.columns))
+
 
         return df_azure_data
 
     except pyodbc.Error as err:
         raise HTTPException(status_code=500, detail="Error connecting to the Azure database")
-#return df_azure_data
-#print(df_azure_data.columns)
+
 
 
 
--- API/main.py
+++ API/main.py
@@ -1,10 +1,85 @@
+
+# @app.post("/predict/")
+# def predict_film_boxoffice(film: FilmInput):
+#     try:
+# data = crud.update_from_azure_db()
+
+
+# df_azure = pd.DataFrame(data, columns=['titre'])
+# print(df_azure)
+
+#         # Créer un DataFrame pandas à partir des données de prédiction
+#         df_prediction = pd.DataFrame(df_azure, columns=["film"])
+
+#         # Utiliser le modèle chargé pour effectuer des prédictions
+#         prediction = model.predict(df_prediction)
+
+#         return {"box_office_prediction": prediction[0]}
+    
+#     except HTTPException as e:
+#         raise e
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail="Error in predicting film box office")
+
+
+
+# @app.post("/predict/")
+# def predict_film_boxoffice(film: FilmInput):
+#     try:
+
+# from fastapi import FastAPI, HTTPException
+# import joblib
+# from pydantic import BaseModel
+# import pandas as pd
+# import crud
+
+# model = joblib.load('pipeml.joblib')
+
+
+# class FilmInput(BaseModel):
+#     titre: str
+
+# data = crud.update_from_azure_db()
+
+# # Instancier un objet de la classe FilmInput pour obtenir le titre du film
+# film_input = FilmInput(titre="The First Slam Dunk")  # Remplacez "Titre du film" par le titre du film réel que vous souhaitez utiliser
+
+# # Récupérez le titre du film à partir de l'objet FilmInput
+# film_titre = film_input.titre
+
+# # Filtrer les données pour garder uniquement les informations concernant le film spécifié
+# film_data = data[data['titre'] == film_titre]
+
+# # cols_to_drop = ['titre', 'acteurs', 'acteurs_connus', 'réalisateur','genres']
+# # X_azure = film_data.drop(cols_to_drop, axis=1)
+# prediction = model.predict(film_data)
+# print(prediction)
+
+#         X_azure = film_data.drop(cols_to_drop, axis=1)
+# #         if film_data.empty:
+#             raise HTTPException(status_code=404, detail="Film non trouvé dans la base de données")
+
+#         # Supprimez la colonne 'titre' car elle a été supprimée pendant l'entraînement
+#         X_azure = film_data.drop(['titre'], axis=1)
+
+#         # Utilisez le modèle chargé et le pipeline de prétraitement pour la prédiction
+#         prediction = pipeline.predict(X_azure)
+
+#         return {"box_office_prediction": prediction[0]}
+    
+#     except HTTPException as e:
+#         raise e
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail="Erreur lors de la prédiction du box-office du film")
 from fastapi import FastAPI, HTTPException
 import joblib
 from pydantic import BaseModel
 import pandas as pd
 import crud
 
-model = joblib.load('modele.pkl')
+# Charger le modèle pré-entraîné
+model = joblib.load('pipeml.joblib')
+
 app = FastAPI()
 
 class FilmInput(BaseModel):
@@ -14,17 +89,23 @@ class FilmInput(BaseModel):
 def predict_film_boxoffice(film: FilmInput):
     try:
         data = crud.update_from_azure_db()
-        df_azure = pd.DataFrame(data, columns=['titre'])
 
-        # Créer un DataFrame pandas à partir des données de prédiction
-        df_prediction = pd.DataFrame(df_azure, columns=["titre"])
+        # Récupérer le titre du film à partir de l'objet FilmInput
+        film_titre = film.titre
 
-        # Utiliser le modèle chargé pour effectuer des prédictions
-        prediction = model.predict(df_prediction)
+        # Filtrer les données pour garder uniquement les informations concernant le film spécifié
+        film_data = data[data['titre'] == film_titre]
 
-        return {"box_office_prediction": prediction[0]}
+        if film_data.empty:
+            raise HTTPException(status_code=404, detail="Film non trouvé dans la base de données")
+
+        # Faire les prédictions avec le modèle chargé
+        prediction = model.predict(film_data)
+
+        return {"box_office_prediction": int(prediction)}
     
     except HTTPException as e:
         raise e
     except Exception as e:
-        raise HTTPException(status_code=500, detail="Error in predicting film box office")
+        raise HTTPException(status_code=500, detail="Erreur lors de la prédiction du box-office du film")
+
